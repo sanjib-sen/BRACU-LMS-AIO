68d1b56f95456b56a83d36e570c319f80f65277c
Sanjib Kumar Sen <sksenonline@gmail.com>
2021-06-08 05:32:21 +0600

    update

---
+++ pages/api/login/[pass].js
@@ -0,0 +1,33 @@
+
+const puppeteer = require("puppeteer");
+
+
+export default async (req, res) =>{
+    res.status(200).json(await getdata(req.query.email,req.query.pass))
+}
+
+async function getdata(email, password){
+
+    const browser = await puppeteer.launch({ headless: false });
+    let page = await browser.newPage();
+
+    await page.goto("https://bux.bracu.ac.bd/login"), 
+    await page.type("#login-email", email, { delay: 30 });
+    await page.type("#login-password", password, { delay: 30 });
+  
+    await page.click("#login > button");
+    await page.waitForNavigation({ waitUntil: "networkidle0" });
+
+    try {
+      await page.$x(
+        "/html/body/div[2]/header/div[1]/div[2]/div/div/div[1]/a/span[1]"
+      );
+    } catch (error) {
+      return undefined;
+    }
+
+    let currentcookies = await page.cookies();
+    browser.close();
+    return currentcookies;
+    
+}
